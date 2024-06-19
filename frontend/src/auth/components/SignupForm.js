748502eac6775ce12e3829cb1ee92c70aec140ab
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-18 20:18:40 -0400

    adds base feature and logics to auth page and loginForm and submitForm components

--- frontend/src/auth/components/SignupForm.js
+++ frontend/src/auth/components/SignupForm.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import tempImage from '../../Placeholder.jpg';
+import Navbar from '../../common/components/Navbar'
+import Footer from '../../common/components/Footer'
+
 
 const SignupForm = () => {
   return (
     <div>
-      <img alt = "Sunflower placeholder" src='https://unsplash.com/photos/yellow-sunflower-in-close-up-photography-YXWoEn5uOvg'></img>
-      <input></input>
+      <Navbar />
+      <h1>WELCOME TO RECIPE CIRCLE</h1>
+      <img width="25%" alt = "Sunflower placeholder" src={tempImage}></img>
+      <label>
+        Email
+        <input type="text"></input>
+      </label>
+      <label>
+        Password
+        <input type="text"></input>
+      </label>
+      <label>
+        Confirm Password
+        <input type="text"></input>
+      </label>
+      <button type="submit">Sign up </button>
+      <Footer />
     </div>
   )
 }
