3e89ea16587cd7d27ee0629d18afc5f87dbeea4d
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-06-22 16:25:21 -0400

    adds preliminary css styling and carousel to landing page

--- frontend/src/landing/components/About/About.js
+++ frontend/src/landing/components/About/About.js
@@ -3,8 +3,13 @@ import './About.css'
 
 const About = () => {
   return (
-    <div>
-        <h2>About Recipe Circle</h2>
+    <div id="about-main-container">
+        <h2 id="about-heading">What is Recipe Circle?</h2>
+        <div id="about-column-container">
+          <div className="about-column">Our Mission</div>
+          <div className="about-column">Create</div>
+          <div className="about-column">Share and Connect</div>
+        </div>
     </div>
   )
 }
