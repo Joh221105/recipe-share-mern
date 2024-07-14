03242a35e1bf6a09dd62493d5e502f9e03332d2d
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-07-13 22:48:19 -0400

    adds controller logic to create a user in the database

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,27 +1,53 @@
+import User from "../models/User";
+import bcrypt from "bcryptjs";
 
 // create user in database
 export const signup = async (req, res) => {
-    try {
+  const { username, email, password } = req.body;
 
-    } catch (error) {
-        console.log(error)
-    }
-  };
-  
-  // handle user login
-  export const login = async (req, res) => {
-    try {
+  try {
+    // Check if user already exists
+    let user = await User.findOne({ email });
 
-    } catch (error) {
-        console.log(error)
+    if (user) {
+      return res
+        .status(400)
+        .json({ message: "User already exists with this email" });
     }
-  };
-  
-  // handle user logout
-  export const logout = async (req, res) => {
-    try {
-      
-    } catch (error) {
-        console.log(error)
-    }
-  };
+
+    // Create new user instance
+    user = new User({
+      username,
+      email,
+      password,
+    });
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    user.password = await bcrypt.hash(password, salt);
+
+    // Save user to database
+    await user.save();
+
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error, try again later" });
+  }
+};
+
+// handle user login
+export const login = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// handle user logout
+export const logout = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log(error);
+  }
+};
