6902baa082e7b23793c3e585612efa7d18a756a4
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-07-17 16:33:50 -0400

    implements user fetch function for userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,27 @@
+import User from '../models/User';
+
 // get user profile
 export const getUserProfile = async (req, res) => {
   try {
+    const userId = req.params.userId;
 
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.json({ user });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).json({ message: 'Server Error' });
   }
 };
+
+
+
+
+
 
 // update user profile
 export const updateUserProfile = async (req, res) => {
