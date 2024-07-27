9544f45fd22d68a5013b84143b6fea766b1964ba
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-07-27 17:36:04 -0400

    imports user profile info into userProfile component, removes the usage of dummy user data

--- frontend/src/profile/components/UserProfile/UserProfile.js
+++ frontend/src/profile/components/UserProfile/UserProfile.js
@@ -1,19 +1,23 @@
-import React from "react";
-import userData from "../../../data/userData";
+import React, { useContext } from "react";
+import { AuthContext } from "../../../context/AuthContext"; 
 import './UserProfile.css';
 
 const UserProfile = () => {
+  const { user } = useContext(AuthContext);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="user-profile-container">
-      <img alt="profile" src={userData[0].profileImage} />
-      <h1>{userData[0].displayName}</h1>
-      <img alt="cover" src={userData[0].coverImg} className="cover-image" />
-      <p>{userData[0].email}</p>
-      <p className="bio">{userData[0].bio}</p>
+      {/* <img alt="profile" src={user.profileImage} />  */}
+      <h1>{user.username}</h1>
+      {/* <img alt="cover" src={user.coverImg} className="cover-image" /> */}
+      <p>{user.email}</p>
+      {/* <p className="bio">{user.biography}</p> */}
     </div>
   );
 };
 
 export default UserProfile;
-
-//Remove user data and use database
