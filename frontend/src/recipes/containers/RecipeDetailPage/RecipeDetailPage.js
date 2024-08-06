2f79718c803e14d035f57a0601f3a90a92cf0d7b
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-08-06 16:18:47 -0400

    removes logic from RecipeDetailPage

--- frontend/src/recipes/containers/RecipeDetailPage/RecipeDetailPage.js
+++ frontend/src/recipes/containers/RecipeDetailPage/RecipeDetailPage.js
@@ -6,29 +6,10 @@ import Footer from "../../../common/components/Footer/Footer";
 import "./RecipeDetailPage.css";
 
 const RecipeDetailPage = () => {
-  const { recipeId } = useParams();
-  const [recipe, setRecipe] = useState(null);
-
-  useEffect(() => {
-    const fetchRecipe = async () => {
-      try {
-        const response = await fetch(
-          `http://localhost:5001/recipes/${recipeId}`
-        );
-        const data = await response.json();
-        setRecipe(data.recipe);
-      } catch (error) {
-        console.error("Error fetching recipe:", error);
-      }
-    };
-
-    fetchRecipe();
-  }, [recipeId]);
-
   return (
     <div>
       <Navbar />
-      {recipe && <RecipeDetail recipe={recipe} />}
+      <RecipeDetail/>
       <Footer />
     </div>
   );
