93ff5018c33220d5471f2ed80204f68667a54a13
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-08-06 16:18:38 -0400

    makes request to backend to retrieve recipe data and display it RecipeDetail

--- frontend/src/recipes/components/RecipeDetail/RecipeDetail.js
+++ frontend/src/recipes/components/RecipeDetail/RecipeDetail.js
@@ -1,7 +1,35 @@
-import React, {useEffect} from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import "./RecipeDetail.css";
 
-const RecipeDetail = ({ recipe }) => {
+const RecipeDetail = () => {
+  const { recipeId } = useParams();
+  const [recipe, setRecipe] = useState(null);
+
+  useEffect(() => {
+    const fetchRecipe = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:5001/recipe/${recipeId}`
+        );
+        if (!response.ok) {
+          throw new Error("Failed to fetch recipe");
+        }
+        const data = await response.json();
+        data.recipe.ingredients = JSON.parse(data.recipe.ingredients);
+        setRecipe(data.recipe);
+      } catch (error) {
+        console.error("Error fetching recipe:", error);
+      }
+    };
+
+    fetchRecipe();
+  }, [recipeId]);
+
+  if (!recipe) {
+    return <div>Loading...</div>;
+  }
+
   const {
     title,
     description,
@@ -14,30 +42,33 @@ const RecipeDetail = ({ recipe }) => {
     directions,
   } = recipe;
 
+  
+  const ingredientsList = ingredients.map((ingredient, index) => (
+    <li key={index}>
+      {ingredient.name}: {ingredient.amount} {ingredient.measurement}
+    </li>
+  ));
+
   return (
     <div className="recipe-details-container">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <p>Author: {author}</p>
       <p>{description}</p>
-      <p>Tags: {tags.join(", ")}</p>
+      <p>Tags: {Array.isArray(tags) ? tags.join(", ") : tags}</p>
       <p>Date Created: {new Date(createdAt).toLocaleDateString()}</p>
       <p>Last Updated: {new Date(updatedAt).toLocaleDateString()}</p>
 
       <h3>Ingredients:</h3>
-      <ul>
-        {ingredients.map((ingredient, index) => (
-          <li key={index}>
-            {ingredient.name}: {ingredient.amount} {ingredient.measurement}
-          </li>
-        ))}
-      </ul>
+      <ul>{ingredientsList}</ul>
 
       <h3>Directions:</h3>
       <ol>
-        {directions.map((step, index) => (
-          <li key={index}>{step}</li>
-        ))}
+        {Array.isArray(directions) && directions.length > 0 ? (
+          directions.map((step, index) => <li key={index}>{step}</li>)
+        ) : (
+          <li>No directions listed</li>
+        )}
       </ol>
     </div>
   );
