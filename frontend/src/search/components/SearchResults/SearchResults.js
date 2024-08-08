b485336b17dda9caa25cae9f159ee11b5744365e
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-08-07 21:57:33 -0400

    displays author name in search results instead of user id

--- frontend/src/search/components/SearchResults/SearchResults.js
+++ frontend/src/search/components/SearchResults/SearchResults.js
@@ -1,20 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchResults.css";
 import RecipeCard from "../../../common/components/RecipeCard/RecipeCard";
 
 const SearchResults = ({ matchingRecipes }) => {
-  let matchingMapResult = matchingRecipes.map((recipe) => {
-    return (
-      <RecipeCard
-        key={recipe.id}
-        title={recipe.title}
-        author={recipe.author}
-        tags={recipe.tags}
-        description={recipe.description}
-        img={recipe.img}
-      />
-    );
-  });
+  const [recipesWithAuthors, setRecipesWithAuthors] = useState([]);
+
+  useEffect(() => {
+    const fetchAuthorNames = async () => {
+      const updatedRecipes = await Promise.all(
+        matchingRecipes.map(async (recipe) => {
+          const userResponse = await fetch(`http://localhost:5001/user/${recipe.author}`);
+          if (!userResponse.ok) {
+            throw new Error("Failed to fetch user profile");
+          }
+          const userData = await userResponse.json();
+          const authorName = userData.user.username;
+
+          return {
+            ...recipe,
+            authorName,
+          };
+        })
+      );
+      setRecipesWithAuthors(updatedRecipes);
+    };
+
+    fetchAuthorNames();
+  }, [matchingRecipes]);
+
+  const matchingMapResult = recipesWithAuthors.map((recipe) => (
+    <RecipeCard
+      key={recipe.id}
+      title={recipe.title}
+      author={recipe.authorName}
+      tags={recipe.tags}
+      description={recipe.description}
+      img={recipe.img}
+    />
+  ));
 
   return <div className="search-results">{matchingMapResult}</div>;
 };
