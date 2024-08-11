d140989507e336c935f485a43d0c35a7815735bf
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-08-11 11:37:39 -0400

    adds comments to code files in the back end

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,35 @@
 import express from "express";
-import bodyParser from "body-parser";
 import cors from "cors";
-import mongoose from "./config/mongoose.js";
+import mongoose from "./config/mongoose.js"; 
 
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/userRoutes.js"; 
 import recipeRoutes from "./routes/recipeRoutes.js";
-import authRoutes from "./routes/authRoutes.js";
+import authRoutes from "./routes/authRoutes.js"; 
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from 'url'; 
 
-const app = express();
+// initialize express app
+const app = express(); 
+
+// gets the file path and directory name for the current module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// middleware to parse JSON request bodies
+app.use(express.json());
+
+// middleware to parse URL-encoded request bodies from forms
+app.use(express.urlencoded({ extended: true }));
+
+// enables CORS to allow requests from different origins
 app.use(cors());
 
+// establishes base routes
 app.use("/user", userRoutes);
 app.use("/recipe", recipeRoutes);
 app.use("/auth", authRoutes);
 
+// serve static files from the "uploads" directory
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.get("/", (req, res) => {
@@ -28,6 +37,7 @@ app.get("/", (req, res) => {
 });
 
 const PORT = process.env.PORT || 5001;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
